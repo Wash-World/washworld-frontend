920889a88698a335a6cc7fee571ca785aa09f47f
Orestis Mamouzelos <ores0029@stud.kea.dk>
2025-05-27 13:53:21 +0200

    Cleared App.tsx and deleted all lower-case prenamed element files

--- App.tsx
+++ App.tsx
@@ -1,42 +1,10 @@
-import Select from "./src/components/elements/Select";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { useState } from "react";
-
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  // Location selection example
-  const [selectedLocation, setSelectedLocation] = useState("");
-
-  // Fetched locations
-  const locations = [
-    { label: "København", value: "1" },
-    { label: "Aarhus", value: "2" },
-    { label: "Odense", value: "3" },
-    { label: "Aalborg", value: "4" },
-    { label: "Esbjerg", value: "5" },
-    { label: "Randers", value: "6" },
-    { label: "Kolding", value: "7" },
-    { label: "Horsens", value: "8" },
-  ];
-
-  const [agreed, setAgreed] = useState(false);
-
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Select dropdown example</Text>
-
-      <Select
-        label="Location"
-        selectedValue={selectedLocation}
-        onValueChange={setSelectedLocation}
-        options={locations}
-        placeholder="Select a location"
-        disabled={false}
-      />
-
-      <Text style={styles.selectedValue}>Selected location id: {selectedLocation}</Text>
-
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -45,27 +13,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    paddingHorizontal: 16,
-    justifyContent: "center",
-  },
-  header: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 16,
-    textAlign: "center",
-  },
-  selectedValue: {
-    marginTop: 12,
-    fontSize: 16,
-    textAlign: "center",
-    backgroundColor: "#fff",
-    paddingHorizontal: 16,
-    justifyContent: "center",
-  },
-  statusText: {
-    fontSize: 16,
-    marginTop: 16,
-    textAlign: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
+
