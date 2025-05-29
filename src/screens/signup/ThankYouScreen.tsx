984e221b0ac9a609f51f96ac578112c93dd7e1ed
sofia amoroso <sofi79a0@stud.kea.dk>
2025-05-29 17:11:45 +0200

    redirect after payment

--- src/screens/signup/ThankYouScreen.tsx
+++ src/screens/signup/ThankYouScreen.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { SignUpStackParamList } from "../../navigation/SignUpNavigator";
@@ -9,15 +9,19 @@ type ScreenProps = NativeStackScreenProps<
   typeof ROUTES.SIGNUP.THANK_YOU
 >;
 
-function ThankYouScreen() {
+function ThankYouScreen({ navigation }: ScreenProps) {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      navigation.replace(ROUTES.LOGIN); // 👈 use replace so user can't go back
+    }, 3000); // 3 seconds delay before redirect
+
+    return () => clearTimeout(timeout);
+  }, [navigation]);
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>THANK YOU FOR YOUR PURCHASE</Text>
-      <Text>
-        We cannot just navigate to Home, because this stack naviation and tab
-        navigations are siblings under the root navigator
-      </Text>
-      <Text>Here we will update the Redux context</Text>
+      <Text>You will be redirected to the login page shortly.</Text>
     </View>
   );
 }
