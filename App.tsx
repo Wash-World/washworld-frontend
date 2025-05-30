715e89092d291fc6a672be1d7e72d600578d8219
JannaJenna <annabarellaalonso@gmail.com>
2025-05-30 17:20:55 +0200

    SafeAreaView

--- App.tsx
+++ App.tsx
@@ -1,17 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
+import { StyleSheet,SafeAreaView } from "react-native";
 import RootNavigator from "./src/navigation/RootNavigator";
 import { Provider } from "react-redux";
 import { store } from "./src/store";
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './src/services/queryClient';
 
+
+
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
+        <SafeAreaView style={styles.container}>
         <RootNavigator />
         <StatusBar style="auto" />
+        </SafeAreaView>
       </Provider>
     </QueryClientProvider>
   );
@@ -20,7 +24,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    //alignItems: "center",
+    //justifyContent: "center",
   },
 });
