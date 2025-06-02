52f583d77d8004510142a5ff4ad33f591ce25cef
kamarini09 <moragiannikam@gmail.com>
2025-06-03 00:25:14 +0200

    add log out button

--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -1,13 +1,39 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { useAppDispatch } from "../../store";
+import { useNavigation } from "@react-navigation/native";
+import { clearAuth } from "../../store/authSlice";
+import { deleteToken } from "../../hooks/secureStore";
+import { ROUTES } from "../../constants/routes";
 
-const ProfileScreen = () => {
+export default function ProfileScreen() {
+  const dispatch = useAppDispatch();
+  const navigation = useNavigation();
+
+  const handleLogout = async () => {
+    //Clear auth state in Redux
+    dispatch(clearAuth());
+
+    // Remove the token from SecureStore
+    await deleteToken();
+
+    //Reset navigation to the Auth flow
+    navigation.reset({
+      index: 0,
+      routes: [{ name: ROUTES.AUTH_STACK }],
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Profile Screen</Text>
+      <Text style={styles.heading}>Profile Screen</Text>
+
+      <TouchableOpacity style={styles.button} onPress={handleLogout}>
+        <Text style={styles.buttonText}>Log Out</Text>
+      </TouchableOpacity>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -15,7 +41,21 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#fff",
+    padding: 20,
+  },
+  heading: {
+    fontSize: 24,
+    marginBottom: 40,
+  },
+  button: {
+    backgroundColor: "#E53E3E", // red-ish “danger” color
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "600",
   },
 });
-
-export default ProfileScreen;
